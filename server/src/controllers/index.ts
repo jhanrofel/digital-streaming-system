63bb9141fe1e1e70fee6cf55125a669657fb503e
Rofel <jhanrofel@gmail.com>
2022-10-29 08:34:49 +0800

    remove rates model, repositories and controller

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -3,7 +3,6 @@ export * from './movies.controller';
 export * from './actors.controller';
 export * from './reviews.controller';
 export * from './links.controller';
-export * from './rates.controller';
 export * from './categories.controller';
 export * from './movies-links.controller';
 export * from './actors-links.controller';
