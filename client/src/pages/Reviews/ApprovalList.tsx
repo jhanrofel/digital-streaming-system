520cd468d9348cef76502bdbbc7c30a6c9e60a66
Rofel <jhanrofel@gmail.com>
2022-11-10 01:22:32 +0800

    refactor duplicate pages on review approval

--- client/src/pages/Reviews/ApprovalList.tsx
+++ client/src/pages/Reviews/ApprovalList.tsx
@@ -1,97 +1,10 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../utilities/hooks";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Stack from "@mui/material/Stack";
-import Tooltip from "@mui/material/Tooltip";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
-import ThumbDownIcon from "@mui/icons-material/ThumbDown";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import {
-  reviewsApproval,
-  reviewsApprovalList,
-} from "../../utilities/slice/reviewSlice";
-import {
-  IReviewFormTable,
-  IReviewFormApprovePost,
-} from "../../utilities/types";
+import { reviewsApprovalList } from "../../utilities/slice/reviewSlice";
+import { IReviewFormTable } from "../../utilities/types";
+import ReviewForm from "../../components/Review/ReviewForm";
 
 const ApprovalList = () => {
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "movie",
-      headerName: "Movie",
-      width: 200,
-      editable: true,
-      renderCell: (params) => {
-        return params.row.reviewMovie.title;
-      },
-    },
-    {
-      field: "description",
-      headerName: "Description",
-      width: 300,
-      editable: true,
-    },
-    {
-      field: "rating",
-      headerName: "Rating",
-      width: 120,
-      editable: true,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      width: 200,
-      editable: true,
-      renderCell: (params) => {
-        return params.row.reviewUser.email;
-      },
-    },
-    {
-      field: "createdAt",
-      headerName: "Post date",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "action",
-      headerName: "Action",
-      width: 150,
-      sortable: false,
-      renderCell: (params) => {
-        const onClickApproved = async () => {
-          const formValues: IReviewFormApprovePost = {
-            id: params.row.id,
-            approval: "approved",
-          };
-          await dispatch(reviewsApproval(formValues));
-        };
-
-        const onClickDisapproved = async () => {
-          const formValues: IReviewFormApprovePost = {
-            id: params.row.id,
-            approval: "disapproved",
-          };
-          await dispatch(reviewsApproval(formValues));
-        };
-
-        return (
-          <IconButton>
-            <Stack spacing={2} direction="row">
-              <Tooltip title="Approved">
-                <ThumbUpIcon color="primary" onClick={onClickApproved} />
-              </Tooltip>
-              <Tooltip title="Disapproved">
-                <ThumbDownIcon color="error" onClick={onClickDisapproved} />
-              </Tooltip>
-            </Stack>
-          </IconButton>
-        );
-      },
-    },
-  ];
   const dispatch = useAppDispatch();
   const rows: IReviewFormTable[] = useAppSelector(
     (state) => state.reviews.data
@@ -101,27 +14,7 @@ const ApprovalList = () => {
     dispatch(reviewsApprovalList());
   }, [dispatch]);
 
-  return (
-    <>
-      <Box sx={{ height: 600, width: "100%" }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[10]}
-          disableSelectionOnClick
-          experimentalFeatures={{ newEditingApi: true }}
-          initialState={{
-            columns: {
-              columnVisibilityModel: {
-                id: false,
-              },
-            },
-          }}
-        />
-      </Box>
-    </>
-  );
+  return <ReviewForm rows={rows} formName={"pendingReview"} />;
 };
 
 export default ApprovalList;
--- client/src/pages/Reviews/ApprovedList.tsx
+++ client/src/pages/Reviews/ApprovedList.tsx
@@ -1,114 +1,20 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../utilities/hooks";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Stack from "@mui/material/Stack";
-import Tooltip from "@mui/material/Tooltip";
-import ThumbDownIcon from "@mui/icons-material/ThumbDown";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import {
-  reviewsApproval,
-  reviewsApprovedList,
-} from "../../utilities/slice/reviewSlice";
-import {
-  IReviewFormTable,
-  IReviewFormApprovePost,
-} from "../../utilities/types";
+import { reviewsApprovedList } from "../../utilities/slice/reviewSlice";
+import { IReviewFormTable } from "../../utilities/types";
+import ReviewForm from "../../components/Review/ReviewForm";
 
 const ApprovedList = () => {
-  const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "movie",
-      headerName: "Movie",
-      width: 200,
-      editable: true,
-      renderCell: (params) => {
-        return params.row.reviewMovie.title;
-      },
-    },
-    {
-      field: "description",
-      headerName: "Description",
-      width: 300,
-      editable: true,
-    },
-    {
-      field: "rating",
-      headerName: "Rating",
-      width: 120,
-      editable: true,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      width: 200,
-      editable: true,
-      renderCell: (params) => {
-        return params.row.reviewUser.email;
-      },
-    },
-    {
-      field: "createdAt",
-      headerName: "Post date",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "action",
-      headerName: "Action",
-      width: 150,
-      sortable: false,
-      renderCell: (params) => {
-
-        const onClickDisapproved = async () => {
-          const formValues: IReviewFormApprovePost = {
-            id: params.row.id,
-            approval: "disapproved",
-          };
-          await dispatch(reviewsApproval(formValues));
-        };
-
-        return (
-          <IconButton>
-            <Stack spacing={2} direction="row">
-              <Tooltip title="Disapproved">
-                <ThumbDownIcon color="error" onClick={onClickDisapproved} />
-              </Tooltip>
-            </Stack>
-          </IconButton>
-        );
-      },
-    },
-  ];
   const dispatch = useAppDispatch();
-  const rows: IReviewFormTable[] = useAppSelector((state) => state.reviews.data);
+  const rows: IReviewFormTable[] = useAppSelector(
+    (state) => state.reviews.data
+  );
 
   useEffect(() => {
     dispatch(reviewsApprovedList());
   }, [dispatch]);
 
-  return (
-    <>
-      <Box sx={{ height: 600, width: "100%" }}>
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[10]}
-          disableSelectionOnClick
-          experimentalFeatures={{ newEditingApi: true }}
-          initialState={{
-            columns: {
-              columnVisibilityModel: {
-                id: false,
-              },
-            },
-          }}
-        />
-      </Box>
-    </>
-  );
+  return <ReviewForm rows={rows} formName={"approveReview"} />;
 };
 
 export default ApprovedList;
