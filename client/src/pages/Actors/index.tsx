6aa135d01f014835aaead5abc96d6efa30832a8c
Rofel <jhanrofel@gmail.com>
2022-11-09 14:35:55 +0800

    add FormTable component for code refactoring

--- client/src/pages/Actors/index.tsx
+++ client/src/pages/Actors/index.tsx
@@ -1,73 +1,19 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import Typography from "@mui/material/Typography";
 import ActorAdd from "./ActorAdd";
 import ActorList from "./ActorList";
+import FormTable from "../../components/FormTable";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
+interface TableTabInfo {
+  label: string;
+  formPages: any;
 }
 
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography component="span">{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
+const tableTab: TableTabInfo[] = [
+  { label: "ACTORS", formPages: <ActorList /> },
+  { label: "ADD", formPages: <ActorAdd /> },
+];
 
 const Actors = () => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  return (
-    <React.Fragment>
-      <Box sx={{ width: "100%", display: "static", marginTop: "100px" }}>
-        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-          >
-            <Tab label="ACTORS" {...a11yProps(0)} />
-            <Tab label="ADD" {...a11yProps(1)} />
-          </Tabs>
-        </Box>
-        <TabPanel value={value} index={0}>
-          <ActorList />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <ActorAdd />
-        </TabPanel>
-      </Box>
-    </React.Fragment>
-  );
+  return <FormTable tableTab={tableTab} />;
 };
 
 export default Actors;
--- client/src/pages/Movies/index.tsx
+++ client/src/pages/Movies/index.tsx
@@ -1,76 +1,21 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
-import Typography from "@mui/material/Typography";
 import Categories from "./Categories";
 import MovieAdd from "./MovieAdd";
 import MovieList from "./MovieList";
+import FormTable from "../../components/FormTable";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
+interface TableTabInfo {
+  label: string;
+  formPages: any;
 }
 
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography component="span">{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
+const tableTab: TableTabInfo[] = [
+  { label: "MOVIES", formPages: <MovieList /> },
+  { label: "ADD", formPages: <MovieAdd /> },
+  { label: "CATEGORY", formPages: <Categories /> },
+];
 
 const Movies = () => {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Box sx={{ width: "100%", display: "static", marginTop: "100px" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="MOVIES" {...a11yProps(0)} />
-          <Tab label="ADD" {...a11yProps(1)} />
-          <Tab label="CATEGORY" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <MovieList />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <MovieAdd />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Categories />
-      </TabPanel>
-    </Box>
-  );
+  return <FormTable tableTab={tableTab} />;
 };
 
 export default Movies;
